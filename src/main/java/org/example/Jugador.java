ba94f28147230520a3d065019f3e751f8b5250a0
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-22 01:54:50 -0300

    cambio en metodos atacar, ahora retorna un objeto Disparo

--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -6,15 +6,17 @@ public class Jugador extends Tanque{
         super(posicion);
         this.nombre = nombre;
     }
-    public void atacar(Direccion direccion){
+    public Disparo atacar(Direccion direccion){
         /*logica de ataque, revisar la de enemigo para
         * polimorfismo*/
         this.cambiarDireccion(direccion);
         Disparo disparo= new Disparo(this.posicion,this.direccion);
+        return disparo;
 
     }
-    public void atacar(){
+    public Disparo atacar(){
         Disparo disparo= new Disparo(this.posicion,this.direccion);
+        return disparo;
     }
 
     public String obtenerNombre(){
