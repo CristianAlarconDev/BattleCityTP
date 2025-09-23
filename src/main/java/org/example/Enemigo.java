a89abc517406e56452685c4f430dc12fcd83990c
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-23 13:07:43 -0300

    estructura de base

--- src/main/java/org/example/Enemigo.java
+++ src/main/java/org/example/Enemigo.java
@@ -1,4 +1,23 @@
 package org.example;
 
-public class Enemigo {
+public class Enemigo extends Tanque{
+    private String tipo;
+
+    Enemigo(String tipo, double x, double y, double velocidadBase) {
+        super(x, y, velocidadBase);
+        this.tipo=tipo;
+        this.direccionActual = Direccion.ABAJO; /*cambiar de ser necesario, sobreescribe
+        a la clase padre*/
+    }
+    public String obtenerTipo(){
+        return tipo;
+    }
+
+    /*idem que jugador, revisar si hay mas logica necesaria y si no llevar
+     metodo a tanque*/
+    public Disparo disparar(){
+        return new Disparo(obtenerPosicion(), obtenerDireccionActual(), obtenerVelocidadBase());
+    }
+
+
 }
