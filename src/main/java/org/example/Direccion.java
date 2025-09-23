1274a30a42cda03a445ca79a08117187a7abe6b3
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-23 03:09:12 -0300

    refactorizacion de direccion, ahora se puede pensar como una direccion del tipo DIRECCION.ARRIBA siendo esto internamente 2 propiedades del tipo double

--- src/main/java/org/example/Direccion.java
+++ src/main/java/org/example/Direccion.java
@@ -1,9 +1,20 @@
 package org.example;
 
 public enum Direccion {
-    ARRIBA,
-    ABAJO,
-    IZQUIERDA,
-    DERECHA
+    ARRIBA(0.0, -1.0),
+    ABAJO(0.0, 1.0),
+    IZQUIERDA(-1.0, 0.0),
+    DERECHA(1.0, 0.0);
+    private final Double coordenadaX;
+    private final Double coordenadaY;
 
+    Direccion(Double coordenadaX, Double coordenadaY) {
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+    }
+
+    public Vector2D comoVector() {
+        return new Vector2D(coordenadaX, coordenadaY);
+    }
 }
+
