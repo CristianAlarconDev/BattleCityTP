8f24bd24755e7a52166d35f620e9a5988ec1bb51
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-23 09:32:54 -0300

    primera version de clase Disparo, agregar casos de uso y refactorizar

--- src/main/java/org/example/Disparo.java
+++ src/main/java/org/example/Disparo.java
@@ -1,14 +1,40 @@
 package org.example;
 
 public class Disparo {
-    private Posicion posicion;
-    private Direccion direccion;
+    private Vector2D posicion;
+    private Vector2D velocidad;
+    private boolean activo;
 
-    Disparo(Posicion posicion, Direccion direccion){
+    public Disparo(Vector2D posicion, Direccion direccion, double velocidadBase) {
         this.posicion = posicion;
-        this.direccion = direccion;
+        this.velocidad = direccion.comoVector();
+        this.velocidad.multiplicarPor(velocidadBase);
+        this.activo = true;
     }
-    /*revisar si esta estructura es necesaria o si es simplemente
-    * anemica*/
 
+    public void mover() {
+        if (!activo) return;
+        posicion.desplazar(velocidad);
+    }
+
+    public Vector2D obtenerPosicion() {
+        return new Vector2D(posicion.obtenerCoordenadaX(),
+                posicion.obtenerCoordenadaY());
+    }
+
+    public boolean estaActivo() {
+        return activo;
+    }
+
+    public boolean desactivar() {
+        this.activo = false;
+        return estaActivo();
+    }
+
+    public void verificarFueraDeLimite(double ancho, double alto) {
+        if (posicion.obtenerCoordenadaX() < 0 || posicion.obtenerCoordenadaX() > ancho ||
+                posicion.obtenerCoordenadaY() < 0 || posicion.obtenerCoordenadaY() > alto) {
+            this.activo = false;
+        }
+    }
 }
