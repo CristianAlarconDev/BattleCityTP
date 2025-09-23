3a1913c7528a7fc4a53b560a9f67003ed09157bd
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-23 09:56:31 -0300

    se agrega caso de uso simple para Disparo clase, rompe la ley de demeter pero se manejara luego en una capa superior

--- src/test/java/org/example/DisparoTest.java
+++ src/test/java/org/example/DisparoTest.java
@@ -1,4 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DisparoTest {
-  
+
+    @Test
+    public void disparoSeMueveEnConVelocidadConstante() {
+
+        Vector2D origen = new Vector2D(0, 0);
+        Disparo disparo = new Disparo(origen, Direccion.DERECHA, 2);
+
+        assert(disparo.obtenerPosicion().esIgualA(new Vector2D(0, 0)));
+
+        disparo.mover();
+        assert(disparo.obtenerPosicion().esIgualA(new Vector2D(2, 0)));
+
+        disparo.mover();
+        assert(disparo.obtenerPosicion().esIgualA(new Vector2D(4, 0)));
+
+    }
+
 }
