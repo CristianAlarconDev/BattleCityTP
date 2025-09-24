1f3770e1c36e232def4d837fc53a18e5867ae4d5
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-24 01:40:51 -0300

    se agregan casos de uso para la clase enemigo, verifican movimiento dado un intervalo de tiempo configurable

---
+++ src/test/java/org/example/EnemigoTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemigoTest {
+
+    @Test
+    public void enemigoNoSeMueveAntesDeIntervalo() throws InterruptedException{
+        Enemigo enemigo = new Enemigo(0, 0,
+                2, 5000);
+
+        Thread.sleep(1000);
+
+
+        assertFalse(enemigo.mover());
+
+    }
+    @Test
+    public void enemigoSeMueveDespuesDeIntervalo() throws InterruptedException{
+        Enemigo enemigo = new Enemigo(0, 0,
+                2, 2000);
+        Thread.sleep(3000);
+
+        assertTrue(enemigo.mover());
+    }
+
+    @Test
+    public void enemigoLuegoDeMoverseTieneOtraPosicion() throws InterruptedException{
+        Enemigo enemigo = new Enemigo(0, 0,
+                2, 2000);
+        Thread.sleep(3000);
+        enemigo.mover();
+        Vector2D ultimaPosicion = enemigo.obtenerPosicion();
+
+        assertFalse(ultimaPosicion.esIgualA(new Vector2D(0,0)));
+    }
+    @Test
+    public void enemigoAlNoMoverseNoCambiaPosicion() throws InterruptedException{
+        Enemigo enemigo = new Enemigo(0, 0,
+                2, 5000);
+        Thread.sleep(1000);
+        enemigo.mover();
+        Vector2D ultimaPosicion = enemigo.obtenerPosicion();
+        assertTrue(ultimaPosicion.esIgualA(new Vector2D(0,0)));
+    }
+    @Test
+    public void enemigoLuegoDeMoverseDebeEsperarParaMoverseNuevamente() throws InterruptedException{
+        Enemigo enemigo = new Enemigo(0, 0,
+                2, 3000);
+        Thread.sleep(3500);
+        boolean movimientoExitoso=enemigo.mover();
+        assertTrue(movimientoExitoso);
+        boolean movimientoFallido=enemigo.mover();
+        assertFalse(movimientoFallido);
+    }
+
+
+}
