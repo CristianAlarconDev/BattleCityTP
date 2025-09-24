0f17fb263112c2b5ec2f0c8a3cc685bf2fe4e3ce
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-24 12:11:50 -0300

    solo imports

---
+++ src/main/java/org/example/Nivel.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nivel {
+    private List<Jugador> jugadores;
+    private List<Enemigo> enemigos;
+    //private List<Bloque> bloques;
+
+    Nivel() {
+        jugadores = new ArrayList<>();
+        enemigos = new ArrayList<>();
+    }
+
+    public void agregarJugador(Jugador jugador) {
+        jugadores.add(jugador);
+    }
+    public boolean moverJugador(int numeroJugador, Direccion direccion) {
+        if (numeroJugador < 0 || numeroJugador > jugadores.size()) {
+            return false;
+        }
+        /*toda la logica de colisiones aca ya que nivel sabe donde estan los bloques*/
+        Jugador jugador = jugadores.get(numeroJugador);
+        jugador.mover(direccion);
+        return true;
+    }
+    public void agregarEnemigo(Enemigo enemigo) {
+        enemigos.add(enemigo);
+    }
+    public void moverEnemigos(){
+        for (Enemigo enemigo : enemigos) {
+            /*toda la logica de colisiones aca ya que nivel sabe donde estan los bloques*/
+
+            enemigo.mover();
+        }
+    }
+}
