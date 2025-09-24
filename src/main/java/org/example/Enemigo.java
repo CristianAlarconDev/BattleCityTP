cb9569c1d78031ee0b8f9a3b432cc65fd4d9e831
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-23 23:36:41 -0300

    primera version de enemigo compuesto por tanque,se evito herencia para manejar el acceso de metodos publicos

--- src/main/java/org/example/Enemigo.java
+++ src/main/java/org/example/Enemigo.java
@@ -1,24 +1,58 @@
 package org.example;
+public class Enemigo {
+    private Tanque tanque;
+    private long ultimoMovimiento;
+    private long ultimoDisparo;
+    private final long INTERVALO_MOVIMIENTO = 1000; // 1 segundo
+    private final long INTERVALO_DISPARO = 2000;    // 2 segundos
+    private Direccion direccionActual;
+    private double velocidad;
 
-public class Enemigo extends Tanque{
-    private String tipo;
+    public Enemigo(double x, double y, double velocidadBase) {
+        tanque = new Tanque(x, y, velocidadBase);
+        ultimoMovimiento = System.currentTimeMillis();
+        ultimoDisparo = System.currentTimeMillis();
+        direccionActual = Direccion.ABAJO;
+        velocidad=tanque.obtenerVelocidadBase();
 
-    Enemigo(String tipo, double x, double y, double velocidadBase) {
-        super(x, y, velocidadBase);
-        this.tipo=tipo;
-        this.direccionActual = Direccion.ABAJO; /*cambiar de ser necesario, sobreescribe
-        a la clase padre*/
-    }
-    public String obtenerTipo(){
-        return tipo;
     }
 
-    /*idem que jugador, revisar si hay mas logica necesaria y si no llevar
-     metodo a tanque*/
-    public Disparo disparar(){
-        return new Disparo(obtenerPosicion(), obtenerDireccionActual(), obtenerVelocidadBase());
+    public void mover() {
+        long tiempoActual = System.currentTimeMillis();
+
+        if (tiempoActual - ultimoMovimiento >= INTERVALO_MOVIMIENTO) {
+            direccionActual = elegirDireccionAleatoria();
+            tanque.mover(direccionActual);
+            ultimoMovimiento = tiempoActual;
+        }
     }
-/*un comentario*/
-/*otro comentario*/
-    /*tercer comentario*/
+
+    public Disparo disparar() {
+        long tiempoActual = System.currentTimeMillis();
+
+        if (tiempoActual - ultimoDisparo >= INTERVALO_DISPARO) {
+            ultimoDisparo = tiempoActual;
+            return new Disparo(obtenerPosicion(),
+                    direccionActual, tanque.obtenerVelocidadBase());
+        }
+        return null;
+    }
+
+
+    public Vector2D obtenerPosicion() {
+        return tanque.obtenerPosicion();
+    }
+
+    public boolean estaVivo() {
+        return tanque.estaVivo();
+    }
+
+    public boolean recibirDanio() {
+        return tanque.recibirDanio();
+    }
+
+    private Direccion elegirDireccionAleatoria() {
+        Direccion[] direcciones = Direccion.values();
+        return direcciones[(int)(Math.random() * direcciones.length)];
+    }
 }
