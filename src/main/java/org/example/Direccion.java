d7ed3a0015e80b4a72da92643e6aeec741933338
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-25 11:37:54 -0300

    refactor de Direccion enum, no cambia funcionalidad

--- src/main/java/org/example/Direccion.java
+++ src/main/java/org/example/Direccion.java
@@ -1,20 +1,19 @@
 package org.example;
 
 public enum Direccion {
-    ARRIBA(0.0, -1.0),
-    ABAJO(0.0, 1.0),
-    IZQUIERDA(-1.0, 0.0),
-    DERECHA(1.0, 0.0);
-    private final Double coordenadaX;
-    private final Double coordenadaY;
+    ARRIBA,
+    ABAJO,
+    IZQUIERDA,
+    DERECHA;
 
-    Direccion(Double coordenadaX, Double coordenadaY) {
-        this.coordenadaX = coordenadaX;
-        this.coordenadaY = coordenadaY;
-    }
 
     public Vector2D comoVector() {
-        return new Vector2D(coordenadaX, coordenadaY);
+        return switch (this) {
+            case ARRIBA -> new Vector2D(0, -1); // si usas JavaFX
+            case ABAJO -> new Vector2D(0, 1);
+            case IZQUIERDA -> new Vector2D(-1, 0);
+            case DERECHA -> new Vector2D(1, 0);
+        };
     }
 }
 
