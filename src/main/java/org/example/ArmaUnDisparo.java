63abfa7bff6d7657f8baddd11f84d1c628ec7ee5
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-25 11:19:08 -0300

    estructura base de la clase ArmaUnDisparo

---
+++ src/main/java/org/example/ArmaUnDisparo.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class ArmaUnDisparo {
+    private Disparo disparoEnCurso;
+    private double velocidadDisparo;
+
+    public ArmaUnDisparo(double velocidadDisparo){
+        disparoEnCurso = null;
+        this.velocidadDisparo=velocidadDisparo;
+    }
+    private void reiniciarDisparo(){
+        if (disparoEnCurso != null && !disparoEnCurso.estaActivo()) {
+            disparoEnCurso = null;
+        }
+    }
+    public boolean puedeDisparar(){
+        reiniciarDisparo();
+        return disparoEnCurso == null;
+    }
+    public Disparo disparar(Vector2D origen, Direccion direccion){
+        if(!puedeDisparar()){
+            throw new IllegalStateException("No se puede disparar aun");
+        }
+        Disparo disparo = new Disparo(origen, direccion, velocidadDisparo);
+        disparoEnCurso = disparo;
+        return disparo;
+    }
+    public void cambiarVelocidadDisparo(double velocidadDisparo){
+        this.velocidadDisparo=velocidadDisparo;
+    }
+    public void disparoFinalizado(){
+        disparoEnCurso = null;
+    }
+    public double obtenerVelocidadDisparo(){
+        return velocidadDisparo;
+    }
+}
