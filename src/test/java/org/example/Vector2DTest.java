05271b8a911ff136f96d6d8802c8bcd7c6f40244
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-25 04:51:51 -0300

    se cambian nombres de metodos, funcionamiento igual

--- src/test/java/org/example/Vector2DTest.java
+++ src/test/java/org/example/Vector2DTest.java
@@ -13,17 +13,17 @@ class Vector2DTest {
     }
     @Test
     public void unVector2DSeAgregaUnaCoordenada(){
-        Vector2D vector2D = new Vector2D(10,10);
-        vector2D.desplazar(new Vector2D(10,10));
-        assertEquals(20,vector2D.obtenerCoordenadaX());
-        assertEquals(20,vector2D.obtenerCoordenadaY());
+        Vector2D vector = new Vector2D(10,10);
+       Vector2D vectorSuma=vector.sumadoA(new Vector2D(10,10));
+        assertEquals(20,vectorSuma.obtenerCoordenadaX());
+        assertEquals(20,vectorSuma.obtenerCoordenadaY());
     }
     @Test
     public void unVector2DSeMultiplicaPorUnFactor(){
         Vector2D vector2D = new Vector2D(10,10);
-        vector2D.multiplicarPor(2);
-        assertEquals(20,vector2D.obtenerCoordenadaX());
-        assertEquals(20,vector2D.obtenerCoordenadaY());
+        Vector2D vectorEscalado=vector2D.escalado(2);
+        assertEquals(20,vectorEscalado.obtenerCoordenadaX());
+        assertEquals(20,vectorEscalado.obtenerCoordenadaY());
     }
     @Test
     public void unVector2DSeInverteCoordenadasX(){
