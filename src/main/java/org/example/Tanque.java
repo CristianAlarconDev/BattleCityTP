294acff8d691a737686413773adf1bda1c5dd2dd
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-25 04:46:04 -0300

    refactorizacion de tanque usando renombre de metodos en vector2d, se cambian nombres de variables para mejor legibilidad; mismo funcionamiento

--- src/main/java/org/example/Tanque.java
+++ src/main/java/org/example/Tanque.java
@@ -6,28 +6,32 @@ public class Tanque {
     protected double velocidadBase;
     protected Direccion direccionActual;
 
-
-    Tanque(double coordenadaX, double coordenadaY, double velocidadBase){
+    public Tanque(double coordenadaX, double coordenadaY, double velocidadBase){
         this.posicion = new Vector2D(coordenadaX,coordenadaY);
         this.vidasTotales = 3;
         this.velocidadBase = velocidadBase;
         this.direccionActual = Direccion.ARRIBA;
-
     }
     protected Direccion obtenerDireccionActual(){
         return direccionActual;
     }
-    public void mover(Direccion direccion){
-        Vector2D desplazamiento =direccion.comoVector();
-        desplazamiento.multiplicarPor(velocidadBase);
-        this.moverA(desplazamiento);
+    public void cambiarDireccion(Direccion direccion){
         this.direccionActual = direccion;
     }
-    private void moverA(Vector2D desplazamiento){
-        posicion.desplazar(desplazamiento);
+    public void mover(Direccion direccion){
+        Vector2D dir =direccion.comoVector();
+        Vector2D desplazamiento = dir.escalado(velocidadBase);
+        this.moverA(desplazamiento);
+        cambiarDireccion(direccion);
     }
-
+    private void moverA(Vector2D desplazamiento){
+        posicion=posicion.sumadoA(desplazamiento);
+    }
+    public void moverSegunDireccionActual(){
+        mover(this.obtenerDireccionActual());
+    }
     public boolean estaVivo(){
+
         return vidasTotales > 0;
     }
 
@@ -43,14 +47,16 @@ public class Tanque {
                 posicion.obtenerCoordenadaY());
     }
     public boolean estaEnPosicion(double coordenadaX, double coordenadaY){
-        return this.posicion.esIgualA(new Vector2D(coordenadaX,coordenadaY));
+        return posicion.esIgualA(new Vector2D(coordenadaX,coordenadaY));
     }
     /*Esto pensado para powerups*/
     public void cambiarVelocidadBase(double velocidadBase){
+
         this.velocidadBase = velocidadBase;
     }
 
     public double obtenerVelocidadBase(){
+
         return velocidadBase;
     }
 
