015f7398e58497d3cb1193ce4acbe4daee1d5e42
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-26 05:32:50 -0300

    estructura de main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,52 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
+      Juego juego = new Juego();
+      juego.iniciarJuego();
 
+       /* try {
+            CargadorDeNivel cargador = new CargadorDeNivel();
+
+            // Busca el archivo XML y XSD en resources/niveles
+            InputStream xml = Main.class.getClassLoader()
+                    .getResourceAsStream("nivel_de_prueba.xml");
+            InputStream xsd = Main.class.getClassLoader()
+                    .getResourceAsStream("levelConfig.xsd");
+
+            Nivel nivel = cargador.cargarNivel(xml, xsd);
+
+
+            System.out.println("Jugadores:");
+            for (Tanque j : nivel.getJugadores()) {
+                System.out.println("- " + j.getClass().getSimpleName()
+                        + " en (" + j.obtenerPosicion().obtenerCoordenadaX()
+                        + "," + j.obtenerPosicion().obtenerCoordenadaY() + ")");
+            }
+
+            System.out.println("Enemigos:");
+            for (Enemigo e : nivel.getEnemigos()) {
+                System.out.println("- " + e.getClass().getSimpleName()
+                        + " en (" + e.obtenerPosicion().obtenerCoordenadaX()
+                        + "," + e.obtenerPosicion().obtenerCoordenadaY() + ")");
+            }
+
+            System.out.println("Bloques en la grilla:");
+            for (int f = 0; f < nivel.getFilas(); f++) {
+                for (int c = 0; c < nivel.getColumnas(); c++) {
+                    Posicion pos = new Posicion(f, c);
+                    Bloque b = nivel.getBloqueEnPosicion(pos);
+                    if (b != null) {
+                        System.out.println("- " + b.getClass().getSimpleName()
+                                + " en (" + f + "," + c + ")");
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
