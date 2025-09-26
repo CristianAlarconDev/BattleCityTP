4b8a4349fa825ce45087a4b7a24d16bf5754ed47
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-26 02:07:24 -0300

    integracion con clase que sumo compañero, Nivel class contiene jugadores y enemigos

--- src/main/java/org/example/Nivel.java
+++ src/main/java/org/example/Nivel.java
@@ -4,55 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Nivel {
-    private List<Jugador> jugadores;
-    private List<Enemigo> enemigos;
-    private int enemigosGeneradosMaximos;
-    private long inicioNivel;
-    private long ultimoSpawn;
-    private long DURACION_NIVEL_MS;
-    private long INTERVAL0_SPAWNS_MS = 20_000;
-    //private List<Bloque> bloques;
+    private static final int FILAS = 30;
+    private static final int COLUMNAS = 40;
 
-    Nivel(int enemigosPorSpawnMaximos, long duracionNivelMS) {
+    private final List<Tanque> jugadores;
+    private final List<Enemigo> enemigos;
+    private final Bloque[][] grilla;
+
+    public Nivel(int cantJugadores){
         jugadores = new ArrayList<>();
         enemigos = new ArrayList<>();
-        enemigosGeneradosMaximos=enemigosPorSpawnMaximos;
-        DURACION_NIVEL_MS=duracionNivelMS;
-
-    }
-    public void cargarNivel(){
-        inicioNivel = System.currentTimeMillis();
-        ultimoSpawn = inicioNivel;
-        /*se generan 2 enemigos al inicio del nivel, luego se puede
-        * refactorizar esto o recibir un parametro para generar mas
-        * enemigos de base*/
-        agregarEnemigo(new Enemigo(10, 10, 2));
-        agregarEnemigo(new Enemigo(20, 20, 2));
-
+        this.grilla = new Bloque[FILAS][COLUMNAS];
     }
 
-
-    public void agregarJugador(Jugador jugador) {
-        jugadores.add(jugador);
-    }
-    public boolean moverJugador(int numeroJugador, Direccion direccion) {
-        if (numeroJugador < 0 || numeroJugador >= jugadores.size()) {
-            return false;
+    public void agregarJugador(Tanque jugador){
+        if (jugadores.size() < 2) {
+            jugadores.add(jugador);
         }
-        /*toda la logica de colisiones aca ya que nivel sabe donde estan los bloques*/
-        Jugador jugador = jugadores.get(numeroJugador);
-        jugador.mover(direccion);
-        return true;
     }
-    public void agregarEnemigo(Enemigo enemigo) {
+
+    public void agregarEnemigo(Enemigo enemigo){
         enemigos.add(enemigo);
     }
-    public void moverEnemigos(){
-        for (Enemigo enemigo : enemigos) {
-            /*toda la logica de colisiones aca ya que nivel sabe donde estan los bloques*/
 
-            enemigo.mover();
+    public void agregarBloque(Bloque bloque){
+        Vector2D pos = bloque.obtenerPosicion();
+        if (pos.obtenerCoordenadaX() >= 0 && pos.obtenerCoordenadaX() < FILAS
+                && pos.obtenerCoordenadaY() >= 0 && pos.obtenerCoordenadaY() < COLUMNAS) {
+            int x = (int) pos.obtenerCoordenadaX();
+            int y = (int) pos.obtenerCoordenadaY();
+            grilla[x][y] = bloque;
         }
     }
-}
 
+    public Bloque getBloqueEnPosicion(Posicion posicion){
+        if (posicion.getCoordenadaX() >= 0 && posicion.getCoordenadaX() < FILAS
+                && posicion.getCoordenadaY() >= 0 && posicion.getCoordenadaY() < COLUMNAS) {
+            return grilla[posicion.getCoordenadaX()][posicion.getCoordenadaX()];
+        }
+        return null;
+    }
+
+    public List<Tanque> getJugadores() {
+        return jugadores;
+    }
+
+    public List<Enemigo> getEnemigos() {
+        return enemigos;
+    }
+
+    public int getFilas(){
+        return FILAS;
+    }
+
+    public int getColumnas(){
+        return COLUMNAS;
+    }
+}
