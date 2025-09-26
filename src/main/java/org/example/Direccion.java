7ca54936d49cdcce94a6a088c1fec0c5239af757
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-26 02:11:41 -0300

    sin cambios, mismo funcionamiento, solo se quito espaciado

--- src/main/java/org/example/Direccion.java
+++ src/main/java/org/example/Direccion.java
@@ -6,10 +6,9 @@ public enum Direccion {
     IZQUIERDA,
     DERECHA;
 
-
     public Vector2D comoVector() {
         return switch (this) {
-            case ARRIBA -> new Vector2D(0, -1); // si usas JavaFX
+            case ARRIBA -> new Vector2D(0, -1);
             case ABAJO -> new Vector2D(0, 1);
             case IZQUIERDA -> new Vector2D(-1, 0);
             case DERECHA -> new Vector2D(1, 0);
