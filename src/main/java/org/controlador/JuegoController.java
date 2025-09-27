0b90bf95b21cd2f2002d641386a85db1bc0f610d
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-27 18:42:46 -0300

    controller para juego y poder pasar datos de vista a modelo

--- src/main/java/org/controlador/JuegoController.java
+++ src/main/java/org/controlador/JuegoController.java
@@ -1,4 +1,13 @@
 package org.controlador;
 
+import javafx.scene.Scene;
+
+import org.modelo.JuegoBattle;
+
 public class JuegoController {
+    private final JuegoBattle juego;
+    public JuegoController(int  cantJugadores) {
+        this.juego = new JuegoBattle(cantJugadores);
+    }
+
 }
