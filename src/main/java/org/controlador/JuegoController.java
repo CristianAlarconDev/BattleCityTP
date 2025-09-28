ac4ea10dabae73c75382efbdd0d6857b3f0016ba
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-28 04:16:07 -0300

    editado el controller para que defina objetos jugador y la vista pueda tomarlos; mas adelante controller debe tener solo la clase juego y ella debe encargarse de devolver algun jugador en particular por int

--- src/main/java/org/controlador/JuegoController.java
+++ src/main/java/org/controlador/JuegoController.java
@@ -2,12 +2,34 @@ package org.controlador;
 
 import javafx.scene.Scene;
 
+import org.modelo.Direccion;
 import org.modelo.JuegoBattle;
+import org.modelo.Jugador;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JuegoController {
-    private final JuegoBattle juego;
+
+    private final List<Jugador> jugadores;
     public JuegoController(int  cantJugadores) {
-        this.juego = new JuegoBattle(cantJugadores);
+        //this.juego = new JuegoBattle(cantJugadores);
+        jugadores = new ArrayList<>();
+        Jugador jugador1=new Jugador("jugador 1", 100, 100, 5);
+        jugadores.add(jugador1);
+        if (cantJugadores == 2){
+            Jugador jugador2=new Jugador("jugador 2", 200, 100, 5);
+            jugadores.add(jugador2);
+        }
+
+    }
+    public List<Jugador> obtenerJugadores(){
+        return this.jugadores;
+    }
+
+    public void moverJugador(int jugador ,Direccion direccion){
+        jugadores.get(jugador).mover(direccion);
+
     }
 
 }
---
+++ src/main/java/org/vista/TableroView.java
@@ -0,0 +1,4 @@
+package org.vista;
+
+public class TableroView {
+}
