6badaa570fe7f54141f993f0e1dcffd784426327
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-28 19:52:40 -0300

    se agrega metodo procesarTeclasActivas para leer siempre las teclas del set

--- src/main/java/org/controlador/InputController.java
+++ src/main/java/org/controlador/InputController.java
@@ -16,25 +16,28 @@ public class InputController {
     }
     public void presionarTecla(KeyCode tecla){
         teclasActivas.add(tecla);
-        procesarTecla(tecla);
+        //procesarTecla(tecla);
     }
     public void  soltarTecla(KeyCode tecla){
+
         teclasActivas.remove(tecla);
     }
 
-    private void procesarTecla(KeyCode tecla){
-        switch (tecla){
-            case W -> juegoController.moverJugador(0, Direccion.ARRIBA);
-            case A ->  juegoController.moverJugador(0,Direccion.IZQUIERDA);
-            case S -> juegoController.moverJugador(0,Direccion.ABAJO);
-            case D-> juegoController.moverJugador(0,Direccion.DERECHA);
+    public void procesarTeclasActivas(){
+        for (KeyCode tecla: teclasActivas){
+            switch (tecla){
+                case W -> juegoController.moverJugador(0, Direccion.ARRIBA);
+                case A ->  juegoController.moverJugador(0,Direccion.IZQUIERDA);
+                case S -> juegoController.moverJugador(0,Direccion.ABAJO);
+                case D-> juegoController.moverJugador(0,Direccion.DERECHA);
 
-            case UP -> juegoController.moverJugador(1, Direccion.ARRIBA);
-            case LEFT ->  juegoController.moverJugador(1,Direccion.IZQUIERDA);
-            case DOWN -> juegoController.moverJugador(1,Direccion.ABAJO);
-            case RIGHT-> juegoController.moverJugador(1,Direccion.DERECHA);
-            case SPACE -> juegoController.jugadorNroDispara(0);
-            case ENTER -> juegoController.jugadorNroDispara(1);
+                case UP -> juegoController.moverJugador(1, Direccion.ARRIBA);
+                case LEFT ->  juegoController.moverJugador(1,Direccion.IZQUIERDA);
+                case DOWN -> juegoController.moverJugador(1,Direccion.ABAJO);
+                case RIGHT-> juegoController.moverJugador(1,Direccion.DERECHA);
+                case SPACE -> juegoController.jugadorNroDispara(0);
+                case ENTER -> juegoController.jugadorNroDispara(1);
+            }
         }
     }
 }
