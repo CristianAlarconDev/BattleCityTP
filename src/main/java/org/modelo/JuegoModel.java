2d347506ab37edffe84e7cfdf52a070283dfa835
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-28 18:09:22 -0300

    version base de JuegoModel solo para la vista y ver comportamiento hasta la version final de la clase Juego

--- src/main/java/org/modelo/JuegoModel.java
+++ src/main/java/org/modelo/JuegoModel.java
@@ -1,4 +1,39 @@
 package org.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JuegoModel {
+    private List<NivelModel> niveles;
+    private int nivelActual;
+    public JuegoModel(int cantJugadores){
+        niveles= new ArrayList<>();
+        nivelActual=0;
+        NivelModel nivelInicial = new NivelModel("jugador 1", "jugador 2");
+        niveles.add(nivelInicial);
+    }
+    private NivelModel obtenerNivelActual(){
+        return niveles.get(nivelActual);
+    }
+    public List<Jugador> obtenerJugadores(){
+        return obtenerNivelActual().obtenerJugadores();
+    }
+    public List<Bloque> obtenerBloques(){
+        return obtenerNivelActual().obtenerBloques();
+    }
+    public List<Enemigo> obtenerEnemigos(){
+        return obtenerNivelActual().obtenerEnemigos();
+    }
+
+    /*acciones*/
+    public void moverJugador(int nroJugador,Direccion direccion){
+        obtenerNivelActual().moverJugador(nroJugador, direccion);
+    }
+    public void jugadorNroDispara(int nroJugador){
+        obtenerNivelActual().jugadorDisparar(nroJugador);
+    }
+    public void actualizar(){
+
+    }
+
 }
