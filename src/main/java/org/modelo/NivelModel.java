f2a3a70a6dea547f8c22f2dc9687769e5df7f7e2
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-29 10:53:49 -0300

    se agregar limites y tamaño de celda; se usan para validar el movimiento de jugador, luego delegar eso o refactorizar

--- src/main/java/org/modelo/NivelModel.java
+++ src/main/java/org/modelo/NivelModel.java
@@ -8,24 +8,44 @@ public class NivelModel {
     private final List<Enemigo> Enemigos;
     private final List<Bloque> bloques;
 
+    private final double ancho;
+    private final double alto;
+    private final double celda;
+
     public NivelModel(String nombreJugador1, String nombreJugador2){
-        this.jugadores=new ArrayList<>();
-        this.Enemigos=new ArrayList<>();
-        this.bloques=new ArrayList<>();
-        jugadores.add(new Jugador(nombreJugador1, 100, 100, 5));
-        if (nombreJugador2!=null){
-            jugadores.add(new Jugador(nombreJugador2, 200, 100, 5));
-        }
+        this(nombreJugador1,nombreJugador2,800,600);
     }
     public NivelModel(String nombreJugador1){
+        this(nombreJugador1,null,800,600);
+
+    }
+    public NivelModel(String nombreJugador1, String nombreJugador2, int ancho, int alto){
         this.jugadores=new ArrayList<>();
         this.Enemigos=new ArrayList<>();
         this.bloques=new ArrayList<>();
+        this.ancho=ancho;
+        this.alto=alto;
+        this.celda=20;
         jugadores.add(new Jugador(nombreJugador1, 100,100,5));
-
+        if (nombreJugador2!=null){
+            jugadores.add(new Jugador(nombreJugador2, 200,100,5));
+        }
     }
-    public void moverJugador(int jugador,Direccion direccion){
-        jugadores.get(jugador).mover(direccion);
+    public void moverJugador(int nroJugador,Direccion direccion){
+        Jugador jugador=jugadores.get(nroJugador);
+        /*chequeo con coord parciales si pasa el limite;
+        mover a alguna clase o delegar esto, refactorizar*/
+        double coordXNueva=(jugador.obtenerCoordenadaX())+
+                (direccion.dX()*jugador.obtenerVelocidadBase());
+        double coordYNueva=(jugador.obtenerCoordenadaY())+
+                (direccion.dY()*jugador.obtenerVelocidadBase());
+
+        if (jugadorDentroDeLimites(coordXNueva,coordYNueva)){
+            jugador.mover(direccion);
+        }
+    }
+    public boolean jugadorDentroDeLimites(double x, double y){
+        return (x>=0&&x<=(ancho-celda))&&(y>=0&&y<=(alto-celda));
     }
     public void jugadorDisparar(int jugador){
         jugadores.get(jugador).intentarDisparar();
