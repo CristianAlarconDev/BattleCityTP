bec8d6b159cd1622b9d14f81dbe5c4ebad874bfb
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-29 01:34:14 -0300

    se cambia a hasmap para gestionar mejor las direcciones activas, de esta forma se van removiendo las direcciones y se evita el movimiento diagonal de la otra version

--- src/main/java/org/controlador/InputController.java
+++ src/main/java/org/controlador/InputController.java
@@ -3,41 +3,46 @@ package org.controlador;
 import javafx.scene.input.KeyCode;
 import org.modelo.Direccion;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class InputController {
     private final JuegoController juegoController;
-    private final Set<KeyCode> teclasActivas;
-
+    private final Map<Integer,Direccion> direccionesActivas;
     public InputController(JuegoController juegoController) {
         this.juegoController = juegoController;
-        teclasActivas= new HashSet<>();
+        direccionesActivas= new HashMap<>();
     }
     public void presionarTecla(KeyCode tecla){
-        teclasActivas.add(tecla);
-        //procesarTecla(tecla);
+        switch (tecla){
+            case W -> direccionesActivas.put(0, Direccion.ARRIBA);
+            case A ->  direccionesActivas.put(0,Direccion.IZQUIERDA);
+            case S -> direccionesActivas.put(0,Direccion.ABAJO);
+            case D-> direccionesActivas.put(0,Direccion.DERECHA);
+
+            case UP -> direccionesActivas.put(1, Direccion.ARRIBA);
+            case LEFT ->  direccionesActivas.put(1,Direccion.IZQUIERDA);
+            case DOWN -> direccionesActivas.put(1,Direccion.ABAJO);
+            case RIGHT-> direccionesActivas.put(1,Direccion.DERECHA);
+            case SPACE -> juegoController.jugadorNroDispara(0);
+            case ENTER -> juegoController.jugadorNroDispara(1);
+        }
     }
     public void  soltarTecla(KeyCode tecla){
-
-        teclasActivas.remove(tecla);
+        switch (tecla){
+            case W,A,S,D -> direccionesActivas.remove(0);
+            case UP,LEFT,DOWN,RIGHT -> direccionesActivas.remove(1);
+        }
     }
 
-    public void procesarTeclasActivas(){
-        for (KeyCode tecla: teclasActivas){
-            switch (tecla){
-                case W -> juegoController.moverJugador(0, Direccion.ARRIBA);
-                case A ->  juegoController.moverJugador(0,Direccion.IZQUIERDA);
-                case S -> juegoController.moverJugador(0,Direccion.ABAJO);
-                case D-> juegoController.moverJugador(0,Direccion.DERECHA);
 
-                case UP -> juegoController.moverJugador(1, Direccion.ARRIBA);
-                case LEFT ->  juegoController.moverJugador(1,Direccion.IZQUIERDA);
-                case DOWN -> juegoController.moverJugador(1,Direccion.ABAJO);
-                case RIGHT-> juegoController.moverJugador(1,Direccion.DERECHA);
-                case SPACE -> juegoController.jugadorNroDispara(0);
-                case ENTER -> juegoController.jugadorNroDispara(1);
-            }
+    public void procesarInputs(){
+        for (Map.Entry<Integer,Direccion> entrada: direccionesActivas.entrySet()){
+            int jugador=entrada.getKey();
+            Direccion direccion=entrada.getValue();
+            juegoController.moverJugador(jugador,direccion);
         }
     }
 }
