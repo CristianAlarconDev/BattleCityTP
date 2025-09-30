e625de33d71232b138d250a4caee3ed51363b035
Ignacio <martinezji@hotmail.com.ar>
2025-09-30 00:23:21 -0300

    se agregó escolisionable

--- src/main/java/org/modelo/Bloque.java
+++ src/main/java/org/modelo/Bloque.java
@@ -1,22 +1,21 @@
 package org.modelo;
 
-public abstract class Bloque {
+public abstract class Bloque  {
     protected Vector2D posicion;
 
     public Bloque(double coordenadaX, double coordenadaY){
         this.posicion = new Vector2D(coordenadaX,coordenadaY);
     }
 
-
     public Vector2D obtenerPosicion(){
         return new Vector2D(posicion.obtenerCoordenadaX(),
                 posicion.obtenerCoordenadaY());
     }
-
     public abstract boolean bloqueaPasoTanque();
     public abstract boolean bloqueaDisparo();
     public abstract boolean esDestructible();
-    public abstract void recibeimpacto();
+    public abstract boolean esColisionable();
+
 
 
 }
