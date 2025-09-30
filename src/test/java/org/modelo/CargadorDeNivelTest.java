0f70b1f31639ac195ab7e5c7371738ffd402cae5
CristianAlarconDev <calarcon@fi.uba.ar>
2025-09-30 11:30:52 -0300

    cambios en test para que imprima entidades de NivelModel por consola

--- src/test/java/org/modelo/CargadorDeNivelTest.java
+++ src/test/java/org/modelo/CargadorDeNivelTest.java
@@ -6,20 +6,25 @@ public class CargadorDeNivelTest {
     public static void main(String[] args) {
         try {
             CargadorDeNivel cargador = new CargadorDeNivel();
-            Nivel nivel = cargador.cargarNivel("nivel_de_prueba.xml", "levelConfig.xsd");
+            NivelModel nivel = cargador.cargarNivel("nivel_de_prueba.xml", "levelConfig.xsd");
 
             System.out.println("Jugadores:");
-            for (Tanque j : nivel.getJugadores()) {
+            for (Tanque j : nivel.obtenerJugadores()) {
                 System.out.println("- " + j.getClass().getSimpleName()+ " en (" + j.obtenerPosicion().obtenerCoordenadaX() + "," + j.obtenerPosicion().obtenerCoordenadaY() + ")");
 
             }
 
             System.out.println("Enemigos:");
-            for (Enemigo e : nivel.getEnemigos()) {
+            for (Enemigo e : nivel.obtenerEnemigos()) {
                 System.out.println("- " + e.getClass().getSimpleName()+ " en (" + e.obtenerPosicion().obtenerCoordenadaX() + "," + e.obtenerPosicion().obtenerCoordenadaY() + ")");
             }
 
             System.out.println("Bloques en la grilla:");
+            for (Bloque b : nivel.obtenerBloques()) {
+                System.out.println("- " + b.getClass().getSimpleName()+ " en (" + b.obtenerPosicion().obtenerCoordenadaX() + "," + b.obtenerPosicion().obtenerCoordenadaY() + ")");
+            }
+
+            /*
             for (int f = 0; f < nivel.getFilas(); f++) {
                 for (int c = 0; c < nivel.getColumnas(); c++) {
                     Posicion pos = new Posicion(f, c);
@@ -28,7 +33,7 @@ public class CargadorDeNivelTest {
                         System.out.println("- " + b.getClass().getSimpleName() + " en (" + f + "," + c + ")");
                     }
                 }
-            }
+            }*/
 
         } catch (Exception e) {
             e.printStackTrace();
