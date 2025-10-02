34f15b90a661ba2e911fba0309018b9bf389e485
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-01 22:04:06 -0300

    se suma clase powerup que hereda de bloque pero no es colisionable por disparos ni impide el paso; devuelve el tipo de power up enum para comportamiento y el tipo de bloque para vista

--- src/main/java/org/modelo/PowerUp.java
+++ src/main/java/org/modelo/PowerUp.java
@@ -1,4 +1,26 @@
 package org.modelo;
 
-public class PowerUp {
+public class PowerUp extends Bloque{
+    private final TipoPowerUp tipoPowerUp;
+    public PowerUp(double coordenadaX, double coordenadaY, TipoPowerUp powerUp) {
+        super(coordenadaX, coordenadaY);
+        tipoPowerUp=powerUp;
+
+    }
+    public TipoBloque obtenerTipo(){
+        return TipoBloque.POWERUP;
+    }
+    @Override
+    public boolean impideElPaso() {
+        return false;
+    }
+    @Override
+    public boolean esColisionable() {
+        /*asi los disparos no se pueden colisionar con los powerups*/
+        return false;
+    }
+    public TipoPowerUp obtenerTipoPowerUp(){
+        return tipoPowerUp;
+    }
+
 }
