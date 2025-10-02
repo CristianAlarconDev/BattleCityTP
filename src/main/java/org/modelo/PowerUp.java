c7072ce02422f0eb4c924b47987e4ee36f93ba49
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-01 22:45:37 -0300

    ahora powerup puede aplicar un efecto a un jugador; casco con jugador.activarCasco o estrella con jugador.activarEstrella

--- src/main/java/org/modelo/PowerUp.java
+++ src/main/java/org/modelo/PowerUp.java
@@ -22,5 +22,11 @@ public class PowerUp extends Bloque{
     public TipoPowerUp obtenerTipoPowerUp(){
         return tipoPowerUp;
     }
-
+    public void aplicarEfecto(Jugador jugador){
+        switch (tipoPowerUp){
+            /*son 10 segundos de casco pero deberiamos ver si debe saberlo powerup para no dejar hardcodeado*/
+            case CASCO -> jugador.activarCasco(10000);
+            case ESTRELLA -> jugador.activarEstrella();
+        }
+    }
 }
