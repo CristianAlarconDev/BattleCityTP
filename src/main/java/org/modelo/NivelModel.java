e39d739461ccbb20a2f8e93f0f4b3a69373cd6f3
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-19 19:20:23 -0300

    logica de movimiento (disparos fuera de limites y verificacion de obstaculos al mover jugador) y colisiones (disparos vs colisionables) se movio casi en su totalidad a las clases gestores, revisar mover logica de limites y choque con objetos que obstaculizan en enemigo para reducir esa clase; falta ademas revisar si generar power up debe ir aca y por ultimo cambiar nombre a clase nivel

--- src/main/java/org/modelo/NivelModel.java
+++ src/main/java/org/modelo/NivelModel.java
@@ -3,33 +3,28 @@ package org.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NivelModel implements EntornoFisico, ContextoDeColision{
+public class NivelModel implements EntornoFisico, ContextoDeColision, ReglasDeNivel, ReglasDeMovimiento{
     private final List<Jugador> jugadores;
     private final List<Enemigo> enemigos;
     private final List<Bloque> bloques;
     private final List<PowerUp>powerUps;
-    private List<Disparo> disparos;
-    private final int tamanioCelda;
-    private final int tamanioDisparo;
+    private final List<Disparo> disparos;
     private final int anchoNivel, altoNivel;
-    private int cantidadDeJugadores;
     private EstadoNivel estadoNivel;
-    private GestorDeMovimientos gestorDeMovimientos;
+    private final GestorDeMovimientos gestorDeMovimientos;
+    private final GestorDeColisiones gestorDeColisiones;
 
-
-    public NivelModel(String nombreJugador1, String nombreJugador2, int ancho, int alto, int cantidadDeJugadores){
+    public NivelModel(int ancho, int alto){
         this.jugadores=new ArrayList<>();
         this.enemigos=new ArrayList<>();
         this.bloques=new ArrayList<>();
         this.disparos=new ArrayList<>();
         this.powerUps=new ArrayList<>();
-        this.tamanioDisparo=6;
-        this.tamanioCelda=20;
         this.anchoNivel=ancho;
         this.altoNivel=alto;
-        this.cantidadDeJugadores=cantidadDeJugadores;
         this.estadoNivel=EstadoNivel.EN_CURSO;
-        this.gestorDeMovimientos= new GestorDeMovimientos(this);
+        this.gestorDeMovimientos= new GestorDeMovimientos(this, this);
+        this.gestorDeColisiones=new GestorDeColisiones(this, this);
     }
     public void agregarBloque(Bloque bloque){
         this.bloques.add(bloque);
@@ -86,7 +81,9 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
     public void moverEnemigos() {
         for (Enemigo enemigo : enemigos) {
            // enemigo.mover( obtenerBloquesColisionables() , anchoNivel,altoNivel, tamanioCelda/2);
-            enemigo.mover(obtenerObstrucciones(), anchoNivel, altoNivel, tamanioCelda/2);
+            /*cambiar aca de donde obtiene enemigo el tamanio de la celda,
+            en jguador delegue a la clase AreaColisionable*/
+            enemigo.mover(obtenerObstrucciones(), anchoNivel, altoNivel, 10);
         }
     }
 
@@ -110,7 +107,7 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
         return this.altoNivel;
     }
 
-    private void intentarGenerarPowerUp(){
+    public void intentarGenerarPowerUp(){
         if (Math.random() < 0.80) {
             TipoPowerUp tipoPowerUp = TipoPowerUp.random();
             double x = Math.random() * anchoNivel;
@@ -119,43 +116,10 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
             this.powerUps.add(powerUp);
         }
     }
-    private void actualizarColisionesConDisparos(){
-        List<Colisionable>colisionables= obtenerColisionables();
-
-        for (Disparo disparo: new ArrayList<>(disparos)) {
-            for (Colisionable colisionable : colisionables){
-                if (disparo.impactaA(colisionable)) {
-                    ResultadoImpacto resultado = colisionable.recibirImpacto(disparo);
-                    if (resultado == ResultadoImpacto.ENEMIGO_ELIMINADO) {
-                        this.enemigos.remove(colisionable);
-                        intentarGenerarPowerUp();
-                    }
-
-                    if (resultado == ResultadoImpacto.DESTRUIDO) {
-                        this.bloques.remove(colisionable);
-                    }
-
-                    if (resultado == ResultadoImpacto.JUGADOR_ELIMINADO) {
-                        this.jugadores.remove(colisionable);
-                    }
-                    if (resultado == ResultadoImpacto.BASE_DESTRUIDA) {
-                        estadoNivel = EstadoNivel.DERROTA;
-                        return;
-                    }
-
-                    disparo.desactivar();
-                    disparos.remove(disparo);
-                    break;
-                }
-            }
-        }
-
-    }
 
     public void actualizarMovimientos(){
-
         moverDisparos();
-        actualizarColisionesConDisparos();
+        gestorDeColisiones.comprobarColisiones();
         gestorDeMovimientos.limpiarDisparosFueraDeLimites();
         moverEnemigos();
         enemigosDisparan();
@@ -177,9 +141,7 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
     }
 
     private void verificarEstadoNivel(){
-        //System.out.println("Verificar estado: enemigos=" + enemigos.size());
         if(this.enemigos.isEmpty()){
-          //  System.out.println("ganaste el nivel");
             this.estadoNivel=EstadoNivel.VICTORIA;
         }
         if (jugadores.isEmpty()){
@@ -192,10 +154,18 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
         try {
            Disparo disparo=jugador.intentarDisparar();
            disparos.add(disparo);
-        }catch (Exception e){
+        }catch (Exception _){
         }
     }
-
+    public void eliminarColisionable(Colisionable colisionable){
+        jugadores.remove(colisionable);
+        enemigos.remove(colisionable);
+        bloques.remove(colisionable);
+    }
+    public void eliminarDisparo(Disparo disparo){
+        disparo.desactivar();
+        disparos.remove(disparo);
+    }
     public boolean enemigoEnMovimiento(Enemigo enemigo){
         return enemigo.enemigoEstaEnMovimiento();
     }
@@ -205,7 +175,7 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
     }
 
     public List<Disparo> obtenerDisparos(){
-        return disparos;
+        return new ArrayList<>(disparos);
     }
     public List<Jugador> obtenerJugadores(){
         return this.jugadores;
@@ -220,6 +190,12 @@ public class NivelModel implements EntornoFisico, ContextoDeColision{
     public List<PowerUp> obtenerPowerUps(){
         return powerUps;
     }
+
+    @Override
+    public void finalizarNivel() {
+        estadoNivel=EstadoNivel.DERROTA;
+    }
+
     public boolean enCurso(){
         return estadoNivel==EstadoNivel.EN_CURSO;
     }
