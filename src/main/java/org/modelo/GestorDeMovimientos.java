6809f3da71c3ebe88ff78123fa8a03ebadd1e997
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-19 19:06:39 -0300

    se corrige bug que provocaba que los disparos no se borraran al llegar al limite, para no romper abstraccion se delega la eliminacion a nivel si es que el gestor verifico el disparo llego al limite

--- src/main/java/org/modelo/GestorDeMovimientos.java
+++ src/main/java/org/modelo/GestorDeMovimientos.java
@@ -1,23 +1,27 @@
 package org.modelo;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class GestorDeMovimientos {
-    private  EntornoFisico nivel;
-     public GestorDeMovimientos(EntornoFisico nivel){
+    private final EntornoFisico nivel;
+    private final ReglasDeMovimiento reglasDeMovimiento;
+
+    public GestorDeMovimientos(EntornoFisico nivel, ReglasDeMovimiento reglasDeMovimiento) {
         this.nivel = nivel;
+        this.reglasDeMovimiento = reglasDeMovimiento;
     }
+
     private boolean estaDentroDeLimites(AreaColisionable areaDePrueba) {
         double xCentro = areaDePrueba.obtenerCentroX();
         double yCentro = areaDePrueba.obtenerCentroY();
-        int semilado = (int)areaDePrueba.obtenerSemilado();
+        double semilado = areaDePrueba.obtenerSemilado();
 
         boolean dentroHorizontal = (xCentro - semilado >= 0) && (xCentro + semilado <= nivel.obtenerAncho());
-        boolean dentroVertical   = (yCentro - semilado >= 0) && (yCentro + semilado <= nivel.obtenerAlto());
+        boolean dentroVertical = (yCentro - semilado >= 0) && (yCentro + semilado <= nivel.obtenerAlto());
 
         return dentroHorizontal && dentroVertical;
     }
+
     private boolean hayColisionConObstaculo(AreaColisionable areaMovimiento) {
         for (Obstruible obstruccion : nivel.obtenerObstrucciones()) {
             if (areaMovimiento.estaEnArea(obstruccion.obtenerAreaColisionable())) {
@@ -26,18 +30,16 @@ public class GestorDeMovimientos {
         }
         return false;
     }
+
     public boolean puedeMoverA(AreaColisionable areaMovimiento) {
         return estaDentroDeLimites(areaMovimiento) && !hayColisionConObstaculo(areaMovimiento);
     }
+
     public void limpiarDisparosFueraDeLimites() {
         List<Disparo> disparos = nivel.obtenerDisparos();
-        Iterator<Disparo> iterador = disparos.iterator();
-        while (iterador.hasNext()) {
-            Disparo disparo = iterador.next();
-            AreaColisionable areaDisparo = disparo.obtenerAreaColisionable();
-            if (!estaDentroDeLimites(areaDisparo)) {
-                disparo.desactivar();
-                iterador.remove();
+        for (Disparo d : disparos) {
+            if (!estaDentroDeLimites(d.obtenerAreaColisionable())) {
+                reglasDeMovimiento.eliminarDisparo(d);
             }
         }
     }
