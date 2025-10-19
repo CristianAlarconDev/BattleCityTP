5b6c84cd792696c191493a397cce4e9f6d9b3cb0
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-18 22:32:27 -0300

    limpieza de interfaz, solo maneja principalmente el resultado de impactar con un disparo y tambien funciona como getter del area de colision que tiene jugador/enemigo/bloque; obs: el nombre de la interfaz puede tener que cambiar a destruible o similar ya que solo se necesita para saber como responde una entidad a disparo

--- src/main/java/org/modelo/Colisionable.java
+++ src/main/java/org/modelo/Colisionable.java
@@ -2,14 +2,13 @@ package org.modelo;
 
 public interface Colisionable {
     ResultadoImpacto recibirImpacto(Disparo disparo);
-    double obtenerCoordenadaX();
-    double obtenerCoordenadaY();
-    boolean impideElPaso();
-    Vector2D obtenerPosicion();
-    /*prueba de uso de nueva clase AreaColisionable*/
-    AreaColisionable obtenerAreaColisionable();
-    default boolean colisionaCon(Colisionable colisionable){
-        return this.obtenerAreaColisionable().estaEnArea(colisionable.obtenerAreaColisionable());
-    }
-}
 
+    double obtenerCoordenadaX();
+
+    double obtenerCoordenadaY();
+
+    Vector2D obtenerPosicion();
+
+    AreaColisionable obtenerAreaColisionable();
+
+}
