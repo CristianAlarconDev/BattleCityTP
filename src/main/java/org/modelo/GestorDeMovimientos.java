d3354f605bbe2a6da4ded9cefd479de5bfaa41c1
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-19 10:18:53 -0300

    gestor de movimientos dada un area ce colision puede verificar si jugador se puede mover a ese lugar, llevar la idea a enemigo, ademas gesto puede borrar los disparos fuera de los limites

--- src/main/java/org/modelo/GestorDeMovimientos.java
+++ src/main/java/org/modelo/GestorDeMovimientos.java
@@ -1,4 +1,44 @@
 package org.modelo;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class GestorDeMovimientos {
+    private  EntornoFisico nivel;
+     public GestorDeMovimientos(EntornoFisico nivel){
+        this.nivel = nivel;
+    }
+    private boolean estaDentroDeLimites(AreaColisionable areaDePrueba) {
+        double xCentro = areaDePrueba.obtenerCentroX();
+        double yCentro = areaDePrueba.obtenerCentroY();
+        int semilado = (int)areaDePrueba.obtenerSemilado();
+
+        boolean dentroHorizontal = (xCentro - semilado >= 0) && (xCentro + semilado <= nivel.obtenerAncho());
+        boolean dentroVertical   = (yCentro - semilado >= 0) && (yCentro + semilado <= nivel.obtenerAlto());
+
+        return dentroHorizontal && dentroVertical;
+    }
+    private boolean hayColisionConObstaculo(AreaColisionable areaMovimiento) {
+        for (Obstruible obstruccion : nivel.obtenerObstrucciones()) {
+            if (areaMovimiento.estaEnArea(obstruccion.obtenerAreaColisionable())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean puedeMoverA(AreaColisionable areaMovimiento) {
+        return estaDentroDeLimites(areaMovimiento) && !hayColisionConObstaculo(areaMovimiento);
+    }
+    public void limpiarDisparosFueraDeLimites() {
+        List<Disparo> disparos = nivel.obtenerDisparos();
+        Iterator<Disparo> iterador = disparos.iterator();
+        while (iterador.hasNext()) {
+            Disparo disparo = iterador.next();
+            AreaColisionable areaDisparo = disparo.obtenerAreaColisionable();
+            if (!estaDentroDeLimites(areaDisparo)) {
+                disparo.desactivar();
+                iterador.remove();
+            }
+        }
+    }
 }
