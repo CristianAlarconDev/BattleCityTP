dad46876c12c07962257b236e3919a670bf584e9
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-21 20:50:49 -0300

    se corrige bug que provocaba que el hud quite espacio al tablero view

--- src/main/java/org/vista/HudView.java
+++ src/main/java/org/vista/HudView.java
@@ -7,18 +7,22 @@ import javafx.scene.media.MediaPlayer;
 public class HudView {
     private BorderPane layout;
     private final MediaPlayer mediaPlayer;
+    private int height;
     public HudView(MediaPlayer mediaPlayer) {
         this.mediaPlayer = mediaPlayer;
+        this.height = 40;
         crearLayout();
     }
     private void crearLayout() {
         layout = new BorderPane();
         layout.setStyle("-fx-background-color: black;");
+        /*el layout del hud quitaba alto del escenario de juego*/
+        layout.setPrefHeight(this.height);
         // --- Botón de Mute ---
         ToggleButton muteButton = new ToggleButton("Silenciar 🔇");
         muteButton.setStyle("-fx-font-size: 12px; -fx-padding: 5; -fx-background-radius: 10;" +
                 "-fx-text-fill: white;-fx-background-color: transparent;");
-
+        /*daba problemas al iniciar, el boton aparecia enfocado y no permitia otras acciones*/
         muteButton.setFocusTraversable(false);
         if (this.mediaPlayer != null) {
             muteButton.setSelected(mediaPlayer.isMute());
@@ -31,5 +35,8 @@ public class HudView {
     }
     public Pane obtenerLayout() {
         return this.layout;
+    }
+    public int obtenerAlto(){
+        return this.height;
     }
 }
