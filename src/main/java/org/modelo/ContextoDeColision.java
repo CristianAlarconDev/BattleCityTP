30194f24b85ebf9929e957201ac0e171558ff536
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-22 20:25:43 -0300

    se agregan getters que va a usar gestor de colision para consultar sus areas y decidir la colision

--- src/main/java/org/modelo/ContextoDeColision.java
+++ src/main/java/org/modelo/ContextoDeColision.java
@@ -5,7 +5,10 @@ import java.util.List;
 public interface ContextoDeColision {
     List<Colisionable> obtenerColisionables();
     List<Disparo> obtenerDisparos();
+    List<PowerUp> obtenerPowerUps();
+    List<Jugador> obtenerJugadores();
     void eliminarColisionable(Colisionable colisionable);
     void eliminarDisparo(Disparo disparo);
     void agregarBloqueTanqueDestruido(Vector2D posicion);
+    void eliminarPowerUp(PowerUp powerUp);
 }
