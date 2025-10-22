bb62dc98df9b6d4676ac8ebe1f1e2f91483f04a5
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-22 02:31:10 -0300

    ahora la interfaz maneja el acceso a jugadores y powerups para verificar su colision en el gestor

--- src/main/java/org/modelo/ContextoDeColision.java
+++ src/main/java/org/modelo/ContextoDeColision.java
@@ -5,6 +5,9 @@ import java.util.List;
 public interface ContextoDeColision {
     List<Colisionable> obtenerColisionables();
     List<Disparo> obtenerDisparos();
+    List<Jugador> obtenerJugadores();
+    List<PowerUp> obtenerPowerUps();
     void eliminarColisionable(Colisionable colisionable);
     void eliminarDisparo(Disparo disparo);
+    void eliminarPowerUp(PowerUp powerUp);
 }
