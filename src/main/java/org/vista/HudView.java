8279e28e21985f2d88802e2fc1f99e9909766c0c
CristianAlarconDev <calarcon@fi.uba.ar>
2025-10-22 01:29:13 -0300

    se agrega label para mostrar los stats de o los jugadores,se suma metodo para actualziar los stats desde array vidas y se mueve l creacion de boton mute a metodo privado

--- src/main/java/org/vista/HudView.java
+++ src/main/java/org/vista/HudView.java
@@ -1,13 +1,21 @@
 package org.vista;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class HudView {
     private BorderPane layout;
     private final MediaPlayer mediaPlayer;
-    private int height;
+    private final int  height;
+    private Label vidasLabel;
+
     public HudView(MediaPlayer mediaPlayer) {
         this.mediaPlayer = mediaPlayer;
         this.height = 40;
@@ -19,6 +27,16 @@ public class HudView {
         /*el layout del hud quitaba alto del escenario de juego*/
         layout.setPrefHeight(this.height);
         // --- Botón de Mute ---
+        ToggleButton muteButton = getMuteButton();
+
+        vidasLabel = new Label();
+        vidasLabel.setFont(Font.font("System", 16));
+        vidasLabel.setTextFill(Color.WHITE);
+        layout.setLeft(vidasLabel);
+        layout.setRight(muteButton);
+    }
+
+    private @NotNull ToggleButton getMuteButton() {
         ToggleButton muteButton = new ToggleButton("Silenciar 🔇");
         muteButton.setStyle("-fx-font-size: 12px; -fx-padding: 5; -fx-background-radius: 10;" +
                 "-fx-text-fill: white;-fx-background-color: transparent;");
@@ -28,10 +46,20 @@ public class HudView {
             muteButton.setSelected(mediaPlayer.isMute());
             muteButton.setOnAction(_ -> mediaPlayer.setMute(muteButton.isSelected()));
         }
+        return muteButton;
+    }
 
-        // --- Stats (Placeholder para el futuro) ---
+    public void actualizarStats(List<Integer> vidas){
+        StringBuilder statsTexto = new StringBuilder();
 
-        layout.setRight(muteButton);
+        if (!vidas.isEmpty()) {
+            statsTexto.append("Jugador 1 Vidas: ").append(vidas.getFirst());
+        }
+        if (vidas.size() >= 2) {
+            statsTexto.append("  |  Jugador 2 Vidas: ").append(vidas.get(1));
+        }
+
+        vidasLabel.setText(statsTexto.toString());
     }
     public Pane obtenerLayout() {
         return this.layout;
